5be7a922cd5fe0da72828a359e13106a0e5adfbc
Arron <arron.m@spacetime.co.nz>
2023-11-04 15:10:15 +1300

    more stuff 🤪

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ import {
   Sprite,
   ScreenElement,
   Text,
-  Loader
+  Loader,
+  GraphicsGroup,
+  AnimationStrategy
 } from 'excalibur'
 import { useEffect, useState, useRef } from 'react'
 import * as ROT from 'rot-js'
@@ -31,12 +33,17 @@ import {
 
 import mapchip from './assets/colored_packed.png'
 import spritemap from './assets/colored_transparent.png'
+import fontTest from './assets/fonttest.png'
+import dialogueBackground from './assets/dialogue_background.png'
+
 import * as LevelRoguelike from 'roguelike/level/roguelike'
 
 export const Resources = {
   mapchip: new ImageSource(mapchip),
   spritemap: new ImageSource(spritemap),
-  dude: new AsepriteResource('/seekers-of-byergur/beard dude 2.json')
+  dude: new AsepriteResource('/seekers-of-byergur/beard dude 5.json'),
+  font: new ImageSource(fontTest),
+  dialogueBackground: new ImageSource(dialogueBackground)
 }
 
 const tileDef = {
@@ -52,32 +59,101 @@ const tileDef = {
 const w = 64
 const h = 48
 
+const SCALE_FACTOR = 2
+const tileSize = 16 * SCALE_FACTOR
+
 class Dialogue extends ScreenElement {
   constructor () {
+    const width = 20 * 20 * 2 - 16
+    const height = 8 * 3 * 2 * 2 * 2 + 32
     super({
-      width: 600,
-      height: 100,
-      x: 0,
-      y: 600 - 100,
-      color: Color.Yellow
-    })
-  }
-
-  onInitialize () {
-    this.graphics.add('idle', Resources.StartButtonBackground)
-    this.graphics.add('hover', Resources.StartButtonHovered)
-
-    this.on('pointerup', () => {
-      alert("I've been clicked")
+      width,
+      height,
+      x: 8,
+      y: 600 - height - 32
     })
 
-    this.on('pointerenter', () => {
-      this.graphics.show('hover')
+    const textStr = 'sphinx of black quartz! judge\nmy vowE! hello world? H'
+    //'soEyou seek the fabled land of byergur?\nwell!~\nyou will have to find it yourselfEH'
+
+    const spriteFontSheet = SpriteSheet.fromImageSource({
+      image: Resources.font,
+      grid: {
+        rows: 3,
+        columns: 26,
+        spriteWidth: 10,
+        spriteHeight: 8 * 3
+      },
+      spacing: {
+        originOffset: {
+          x: 0,
+          y: 0
+        },
+        margin: {
+          x: 0,
+          y: 0
+        }
+      }
     })
 
-    this.on('pointerleave', () => {
-      this.graphics.show('idle')
+    const spriteFontSheetWide = SpriteSheet.fromImageSource({
+      image: Resources.font,
+      grid: {
+        rows: 3,
+        columns: 13,
+        spriteWidth: 20,
+        spriteHeight: 8 * 3
+      },
+      spacing: {
+        originOffset: {
+          x: 0,
+          y: 8 * 6
+        },
+        margin: {
+          x: 0,
+          y: 0
+        }
+      }
     })
+
+    const scriptFont = new SpriteFont({
+      alphabet: 'ABDEGHIKLMNOPRSTU ',
+      caseInsensitive: false,
+      spriteSheet: spriteFontSheetWide
+    })
+
+    const spriteFont = new SpriteFont({
+      alphabet: 'abcdefghijklmnopqrstuvwxyz .?!H~SE',
+      caseInsensitive: false,
+      spriteSheet: spriteFontSheet
+    })
+
+    const text = new Text({
+      text: textStr,
+      font: spriteFont,
+      scale: new Vector(2, 2)
+    })
+
+    const script = new Text({
+      text: 'HELLO SERGIO\nTHIS IS A TEST',
+      font: scriptFont,
+      scale: new Vector(2, 2)
+    })
+
+    const dialogueBackgroundSprite = new Sprite({
+      image: Resources.dialogueBackground,
+      scale: new Vector(2, 2)
+    })
+
+    const group = new GraphicsGroup({
+      members: [
+        { graphic: dialogueBackgroundSprite, pos: new Vector(0, 16) },
+        { graphic: text, pos: new Vector(68, 32 + 8 * 3 * 2 * 2) }
+        //{ graphic: script, pos: new Vector(56, 32 + 8 * 3 * 2 * 2) }
+      ]
+    })
+    //this.graphics.add(text)
+    this.graphics.add(group)
   }
 }
 
@@ -97,9 +173,6 @@ function buildRoom () {
   })
 }
 
-const SCALE_FACTOR = 6
-const tileSize = 16 * SCALE_FACTOR
-
 const loader = new Loader([...Object.values(Resources)])
 
 function App () {
@@ -110,7 +183,7 @@ function App () {
         width: 800,
         height: 600,
         canvasElement: gameRef.current,
-        displayMode: DisplayMode.FillScreen,
+        displayMode: DisplayMode.FitScreen,
         backgroundColor: Color.Black
       })
       game.start(loader).then(() => {
@@ -134,39 +207,6 @@ function App () {
           }
         })
 
-        const spriteFontSheet = SpriteSheet.fromImageSource({
-          image: Resources.spritemap,
-          grid: {
-            rows: 4,
-            columns: 13,
-            spriteWidth: 13,
-            spriteHeight: 16
-          },
-          spacing: {
-            originOffset: {
-              x: 35 * 16,
-              y: 17 * 16
-            },
-            margin: {
-              x: 3,
-              y: 0
-            }
-          }
-        })
-
-        const spriteFont = new SpriteFont({
-          alphabet: '0123456789:.%abcdefghijklmnopqrstuvwxyz#+-*/=@ ',
-          caseInsensitive: true,
-          spriteSheet: spriteFontSheet
-        })
-
-        const text = new Text({
-          text:
-            'ahh.. you seek the fabled land of byergur.\n well.. you will have to find it yourself...',
-          font: spriteFont,
-          scale: new Vector(2, 2)
-        })
-
         const roomSprite = spritesheet.getSprite(4, 0)
 
         const { world: map, enter, rooms } = buildRoom()
@@ -188,29 +228,33 @@ function App () {
           tileDef.FLOOR
         ]
 
+        const doorSprite = spritesheet.getSprite(1, 9)
+
+        const wallSprites = {
+          topLeft: spritesheet.getSprite(18, 0),
+          top: spritesheet.getSprite(19, 0),
+          topRight: spritesheet.getSprite(20, 0),
+
+          left: spritesheet.getSprite(18, 1),
+          right: spritesheet.getSprite(20, 1),
+
+          middle: spritesheet.getSprite(10, 17),
+
+          bottomLeft: spritesheet.getSprite(18, 2),
+          bottom: spritesheet.getSprite(19, 2),
+          bottomRight: spritesheet.getSprite(20, 2)
+        }
+        Object.keys(wallSprites).forEach(wall => {
+          wallSprites[wall].scale = new Vector(SCALE_FACTOR, SCALE_FACTOR)
+        })
+
         map.forEach((cols, col) => {
           cols.forEach((cell, row) => {
-            const grass = ROT.RNG.getItem([
-              3,
-              4,
-              3,
-              4,
-              3,
-              4,
-              3,
-              4,
-              3,
-              4,
-              3,
-              4,
-              5,
-              6,
-              7
-            ])
+            const grass = ROT.RNG.getItem([0, 0, 0, 0, 0, 0, 1, 1, 1, 6, 7])
             const trees = ROT.RNG.getItem([2, 2, 2, 2, 3])
             const grassSprite = spritesheet.getSprite(grass, 0)
             const treesSprite = spritesheet.getSprite(trees, 1)
-            const doorSprite = spritesheet.getSprite(1, 9)
+
             const spriteScale = new Vector(SCALE_FACTOR, SCALE_FACTOR)
             grassSprite.scale = spriteScale
             treesSprite.scale = spriteScale
@@ -219,9 +263,12 @@ function App () {
             if (tile) {
               const solid = !passable.includes(cell)
               const door = cell === tileDef.ENTER
-              tile.addGraphic(
-                door ? doorSprite : solid ? treesSprite : grassSprite
-              )
+
+              if (cell === tileDef.WALL) {
+                tile.addGraphic(wallSprites.middle)
+              } else {
+                tile.addGraphic(door ? doorSprite : grassSprite)
+              }
               tile.solid = solid
             }
           })
@@ -231,11 +278,17 @@ function App () {
         const player = new Dude(spawnPos)
         game.add(tilemap)
 
-        player.on('precollision', () => {
+        /*player.on('precollision', () => {
           player.actions.clearActions()
+        })*/
+
+        game.input.keyboard.on('press', evt => {
+          if (evt.key === 'KeyE') {
+            player.wave()
+            return
+          }
         })
 
-        // control player with wasd keys
         game.input.keyboard.on('hold', evt => {
           const speed = 10
           const dir = new Vector(0, 0)
@@ -255,6 +308,11 @@ function App () {
           }
           if (dir.x !== 0 || dir.y !== 0) {
             player.actions.clearActions()
+            // normalise so we don't move faster diagonally
+            dir.normalize()
+
+            //player.actions.moveBy(dir.x * tileSize, dir.y * tileSize, 100)
+
             player.vel.x += dir.x * speed
             player.vel.y += dir.y * speed
             //player.actions.moveBy(dir.x * tileSize, dir.y * tileSize, speed)
@@ -288,16 +346,22 @@ function App () {
 
           //player.rotation = angle
           //player.actions.rotateBy(angle - player.rotation, Math.PI, RotationType.ShortestPath)
-          player.actions.moveTo(destination, 100)
+          //player.actions.moveTo(destination, 100)
           // /player.vel.y = 0.1 * player.pos.y - event.worldPos.y
         })
 
         //player.graphics.use(text)
 
+        /*game.add(new Dude(spawnPos.add(new Vector(-1, 0))))
+        game.add(new Dude(spawnPos.add(new Vector(1, 0))))
+        game.add(new Dude(spawnPos.add(new Vector(0, 1))))
+        game.add(new Dude(spawnPos.add(new Vector(1, 1))))
+        */
         game.add(player)
+        game.add(new Snek(spawnPos.add(new Vector(-2, 2))))
         game.currentScene.camera.strategy.lockToActor(player)
 
-        const dialogue = new Dialogue()
+        //const dialogue = new Dialogue()
         //game.add(dialogue)
       })
     }
@@ -306,25 +370,68 @@ function App () {
   return <canvas ref={gameRef}></canvas>
 }
 
+class Snek extends Actor {
+  constructor (spawnPos) {
+    super({
+      x: spawnPos.x * tileSize + tileSize / 2,
+      y: spawnPos.y * tileSize + tileSize / 2,
+      width: 8,
+      height: 8,
+      color: Color.White,
+      scale: new Vector(SCALE_FACTOR, SCALE_FACTOR),
+      collisionType: CollisionType.Fixed
+    })
+
+    this.idleAnim = Resources.dude.getAnimation('snek')
+    this.graphics.use(this.idleAnim)
+  }
+}
+
 class Dude extends Actor {
   constructor (spawnPos) {
     super({
       x: spawnPos.x * tileSize + tileSize / 2,
       y: spawnPos.y * tileSize + tileSize / 2,
-      width: 1,
-      height: 1,
+      width: 8,
+      height: 8,
       color: Color.White,
-      scale: new Vector(SCALE_FACTOR, SCALE_FACTOR)
+      scale: new Vector(SCALE_FACTOR, SCALE_FACTOR),
+      collisionType: CollisionType.Active
     })
+
     this.idleAnim = Resources.dude.getAnimation('idle')
     this.frontAnim = Resources.dude.getAnimation('front walk')
     this.sideAnim = Resources.dude.getAnimation('side walk')
     this.backAnim = Resources.dude.getAnimation('back run')
+    this.waveAnim = Resources.dude.getAnimation('wave')
 
+    this.waveAnim.strategy = AnimationStrategy.End
     this.graphics.use(this.idleAnim)
+    this.graphics.offset = new Vector(0, -8)
+    this.waving = false
+
+    this.vel = new Vector(0, 0)
   }
+
+  wave () {
+    this.waving = true
+    this.waveAnim.reset()
+  }
+
   onPreUpdate () {
-    const { vel } = this
+    const { vel, waving } = this
+
+    if (waving && vel.x === 0 && vel.y === 0) {
+      this.graphics.use(this.waveAnim)
+      this.waveAnim.events.on('end', () => {
+        this.waving = false
+        this.graphics.use(this.idleAnim)
+      })
+      return
+    } else if (waving) {
+      // cancel animation if we moved while waving
+      this.waving = false
+    }
 
     if (vel.x !== 0) {
       this.graphics.use(this.sideAnim)
@@ -339,43 +446,21 @@ class Dude extends Actor {
     }
     if (vel.x === 0 && vel.y === 0) this.graphics.use(this.idleAnim)
 
-    if (this.vel.x < 5 && this.vel.x > -5) this.vel.x = 0
-    if (this.vel.y < 5 && this.vel.y > -5) this.vel.y = 0
+    const th = 10
+    if (this.vel.x < th && this.vel.x > -1 * th) this.vel.x = 0
+    if (this.vel.y < th && this.vel.y > -1 * th) this.vel.y = 0
 
     this.vel.x = this.vel.x * 0.9
     this.vel.y = this.vel.y * 0.9
-  }
-}
 
-// enemy class
-class Enemy extends Actor {
-  constructor () {
-    this.hp = 100
-    this.maxHp = 100
-    this.attack = 10
-    this.defense = 10
-    this.speed = 10
-    this.exp = 10
-    this.gold = 10
-    this.name = 'Enemy'
-    this.sprite = null
-    this.spriteScale = new Vector(2, 2)
-    this.spriteOffset = new Vector(0, 0)
-    this.pos = new Vector(0, 0)
-    this.width = 1
-    this.height = 1
-    this.color = Color.Red
-    this.collisionType = CollisionType.Active
-    this.body = new BodyComponent({
-      collider: new Collider({
-        mass: 1000,
-        friction: 0.9
-      })
-    })
-  }
+    this.pos.x = Math.round(this.pos.x)
+    this.pos.y = Math.round(this.pos.y)
 
-  onPreUpdate (engine, delta) {
-    super.onPreUpdate(engine, delta)
+    //center the player on the tile
+    /*if (vel.x === 0 && vel.y === 0) {
+      this.pos.x = Math.round(this.pos.x / tileSize) * tileSize
+      this.pos.y = Math.round(this.pos.y / tileSize) * tileSize
+    }*/
   }
 }
 
